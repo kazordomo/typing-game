214c676227f56669a56b5d4e55e4022de7013e91
Zak Eklund <zak.eklund@live.se>
2017-02-12 19:40:20 +0100

    register done in es5. error in es6, next step.

--- models/user.js
+++ models/user.js
@@ -20,40 +20,89 @@ let UserSchema = new mongoose.Schema({
     }
 });
 
-UserSchema.statics.authenticate = (email, password, callback) => {
+/*  REGULAR
+ ------------------------------------------------------------*/
+UserSchema.statics.authenticate = function(email, password, callback) {
     User.findOne({ email: email })
-        .exec((error, user) => {
-            if(error) {
+        .exec(function (error, user) {
+            if (error) {
                 return callback(error);
-            } else if(!user) {
-                let err = new Error('User not found');
+            } else if ( !user ) {
+                var err = new Error('User not found.');
                 err.status = 401;
                 return callback(err);
             }
-            bcrypt.compare(password, user.password, (err, result) => {
-                if(result === true) {
+            //plain text password, hashed password and a callback function
+            bcrypt.compare(password, user.password, function(err, result) {
+                if (result === true) {
+                    //the callbacks first argument is error. if no error, return error == null
                     return callback(null, user);
                 } else {
                     return callback();
                 }
             });
         });
-};
+}
 
-// Hash password before saving to db with pre-save-hook
-// Before mongo saves to db, this function will run
-UserSchema.pre('save', (next) => {
-    let user = thisM
+UserSchema.pre('save', function(next) {
+    //user object and ts data
+    var user = this;
+    console.log(user);
     //replace the plain text pass with the hash
-    bcrypt.hash(user.password, 10, (err, hash) => {
-        if(err) {
+    bcrypt.hash(user.password, 10, function(err, hash) {
+        if (err) {
             return next(err);
         }
         user.password = hash;
+        //calls the next middleware in the stack
         next();
     });
 });
-
-// Add the scehma to mongo
-let User = mongoose.model('User', UserSchema);
+// add the schema to mongo
+var User = mongoose.model('User', UserSchema);
 module.exports = User;
+
+
+/*  ES6, THROWS ERROR. FIX PL0X
+ ------------------------------------------------------------*/
+
+// UserSchema.statics.authenticate = function(email, password, callback) {
+//     User.findOne({ email: email })
+//         .exec(function (error, user) {
+//             if (error) {
+//                 return callback(error);
+//             } else if ( !user ) {
+//                 var err = new Error('User not found.');
+//                 err.status = 401;
+//                 return callback(err);
+//             }
+//             //plain text password, hashed password and a callback function
+//             bcrypt.compare(password, user.password, function(err, result) {
+//                 if (result === true) {
+//                     //the callbacks first argument is error. if no error, return error == null
+//                     return callback(null, user);
+//                 } else {
+//                     return callback();
+//                 }
+//             });
+//         });
+// }
+//
+// // Hash password before saving to db with pre-save-hook
+// // Before mongo saves to db, this function will run
+// UserSchema.pre('save', (next) => {
+//     let user = this;
+//     console.log(user);
+//     //replace the plain text pass with the hash
+//     bcrypt.hash(user.password, 10, (err, hash) => {
+//         if(err) {
+//             return next(err);
+//         }
+//         user.password = hash;
+//         next();
+//     });
+// });
+//
+// // Add the scehma to mongo
+// let User = mongoose.model('User', UserSchema);
+// module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,6 @@ const User = require('../models/user');
 //the index.js file is called automatically from the folder.
 const mid = require('../middleware');
 
-// GET /login
-//user the middleware exported from middleware/index.js
-router.get('/login', mid.loggedOut, (req, res, next) => {
-    return res.render('login', { title: 'Login' });
-});
-
 // GET /logout
 router.get('/logout', (req, res, next) => {
     if (req.session) {
@@ -22,6 +16,12 @@ router.get('/logout', (req, res, next) => {
             }
         });
     }
+});
+
+// GET /login
+//user the middleware exported from middleware/index.js
+router.get('/login', mid.loggedOut, (req, res, next) => {
+    return res.render('login', { title: 'Login' });
 });
 
 // POST /login
@@ -49,41 +49,84 @@ router.post('/login', (req, res, next) => {
 
 // GET /register
 router.get('/register', mid.loggedOut, (req, res, next) => {
-    return res.render('register', { title: 'Register' });
+    return res.render('register', { title: 'SIgn Up' });
 });
 
+/*  ES6, THROWS ERROR. FIX PL0X
+ ------------------------------------------------------------*/
+
 // POST /register
-//the view action is set to register, which handles the register and then redirects
-//to the game view when done.
-router.post('register', (req, res, next) => {
-    //check for null values, using the name="" from the view
-    if(req.body.email && req.body.name && req.body.password && req.body.confirmPassword) {
-        //confirm password
-        if(req.body.password !== req.body.confirmPassword) {
-            let err = new Error('Passwords do not match');
+// the view action is set to register, which handles the register and then redirects
+// to the game view when done.
+// router.post('/register', (req, res, next) => {
+//     //check for null values, using the name="" from the view
+//     if(req.body.email && req.body.name && req.body.password && req.body.confirmPassword) {
+//         //confirm password
+//         if(req.body.password !== req.body.confirmPassword) {
+//             let err = new Error('Passwords do not match');
+//             err.status = 400;
+//             return next(err);
+//         }
+//
+//         //create object with form input
+//         let userData = {
+//             email: req.body.email,
+//             name: req.body.name,
+//             password: req.body.password
+//         }
+//
+//         //use the schemas 'create' method to insert document into Mongo
+//         User.create(userData, (err, user) => {
+//             if(err) {
+//                 return next(err);
+//             } else {
+//                 //when registered -> logged in
+//                 res.session.userId = user._id;
+//                 return res.redirect('/game');
+//             }
+//         });
+//     } else {
+//         let err = new Error('All fields required');
+//         err.status = 400;
+//         return next(err);
+//     }
+// });
+
+/*  REGULAR
+ ------------------------------------------------------------*/
+router.post('/register', function(req, res, next) {
+    // check that all information has value
+    if (req.body.email &&
+        req.body.name &&
+        req.body.password &&
+        req.body.confirmPassword) {
+
+        // confirm that user typed same password twice
+        if (req.body.password !== req.body.confirmPassword) {
+            var err = new Error('Passwords do not match');
             err.status = 400;
             return next(err);
         }
 
-        //create object with form input
-        let userData = {
+        // create object with form input
+        var userData = {
             email: req.body.email,
             name: req.body.name,
             password: req.body.password
-        }
+        };
 
-        //use the schemas 'create' method to insert document into Mongo
-        User.create(userData, (err, user) => {
-            if(err) {
-                return next(err);
+        // use schema's 'create' method to insert document intro Mongo
+        User.create(userData, function(error, user) {
+            if (error) {
+                return next(error);
             } else {
-                //when registered -> logged in
-                res.session.userId = user._id;
+                //when they register, they are automatically logged in.
+                req.session.userId = user._id;
                 return res.redirect('/game');
             }
         });
     } else {
-        let err = new Error('All fields required');
+        var err = new Error('All fields required');
         err.status = 400;
         return next(err);
     }
