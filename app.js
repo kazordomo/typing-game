3484d9ae061e049500505de0e83eea481b4641c3
Zak Eklund <Zak Eklund>
2017-05-18 14:33:13 +0200

    no comments.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const favicon = require('serve-favicon');
+const path = require('path');
 const app = express();
 
 //heroku
